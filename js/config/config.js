2de6af14ffe1b1627eb76c79f190accff2fe0667
syncdecodes <syncdecodes@gmail.com>
2026-02-12 20:50:27 +0530

    Update API_BASE_URL to new production endpoint

--- js/config/config.js
+++ js/config/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:5000/api";
-// export const API_BASE_URL = "https://e-commerce-backend-kjdf.onrender.com";
+// export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://e-commerce-backend-ujma.onrender.com";
